4dcb84dbb96d1e86dcf7add45701b3f4c9c3e8d9
D. Williams <d.williams@posteo.net>
2020-09-05 16:17:51 +0200

    Remove unneeded part of the headline regexp.

--- org-starless.el
+++ org-starless.el
@@ -35,7 +35,7 @@
   nil nil nil
   :require 'org
   (let* ((keyword
-          `(("^\\(\\*+ \\).+"
+          `(("^\\(\\*+ \\)"
              (1 (put-text-property (match-beginning 1) (match-end 1) 'invisible t)
                 nil)))))
     (if org-starless-mode
